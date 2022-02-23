a076478a5bcc7cfc1d617d186d1a07867948e545
fabpico <fabian.picone@gmail.com>
2022-02-23 11:18:50 +0100

    Prepare reproduction of non working autocomplete-import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {FooComponent} from '@fabpico/test-node-autocomplete-library/dist/components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // try to autocomplete-import FooComponent that resides in '@fabpico/test-node-autocomplete-library/dist/components'
+    return <div>
+        <FooComponent/>
+    </div>;
 }
 
 export default App;
